51be8576e6fc6e62d620cfdb28fd493a606384c1
spider-sauce <james.tacker@saucelabs.com>
2021-02-03 11:43:58 -0800

    first commit

---
+++ cypress/page-objects/SwagOverviewPage.js
@@ -0,0 +1,8 @@
+class SwagOverviewPage {
+    // Make it private so people can't mess with it
+    // Source: https://github.com/tc39/proposal-class-fields#private-fields
+    get screen() {
+        return cy.get('.inventory_list');
+    }
+}
+export default new SwagOverviewPage();
